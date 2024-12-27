aa42cbc8304903c41f681b9e4d7c9158dc4b928f
ArctisTor <cplatz@citadel.edu>
2024-12-27 11:54:00 -0500

    Day 1 complete. Not very good bootcamp.

--- react-bc/src/components/Course.js
+++ react-bc/src/components/Course.js
@@ -1,12 +1,38 @@
 import React from "react";
 import Future from "./Future";
+import { useState } from "react";
 
 function Course(prop) {
+
+  const [mostPopularChecked, setMostPopularChecked] = useState(false);
+  const [leastPopularChecked, setLeastPopularChecked] = useState(false);
+
+  const handleOnChange = (event) => {
+    //console.log(`${event.target.id} checkbox was clicked: ${event.target.checked}`);
+    if (event.target.id === 'mostPopular') {
+      setMostPopularChecked(event.target.checked)
+    } else if (event.target.id === 'leastPopular') {
+      setLeastPopularChecked(event.target.checked)
+    } else {
+      console.log('Unknown');
+    }
+  };
+
+
+
   return (
     <>
-      <div>Most Popular: {prop.courses.mostPopular}</div>
-      <div>Least Popular: {prop.courses.leastPopular}</div>
-      <Future futureCourse={prop.courses.futureCourse}/>
+      <div>
+        <p>
+          Most Popular Course Name: {prop.courses.mostPopular}
+          <input id="mostPopular" checked={mostPopularChecked} type="checkbox" onChange={handleOnChange}/>
+        </p>
+        <p>
+          Least Popular Course Name: {prop.courses.leastPopular}
+          <input id="leastPopular" checked={leastPopularChecked} type="checkbox" onChange={handleOnChange}/>
+        </p>
+        <Future futureCourse={prop.courses.futureCourse} />
+      </div>
     </>
   );
 }
